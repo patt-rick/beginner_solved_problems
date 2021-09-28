fadf546435e9d583ed36c0a2ac1758a11f4b68b0
minnez <patrickackom003@gmail.com>
2021-09-28 10:50:29 +0000

    fixed errors in hangman and added the dictionary

--- hangman.py
+++ hangman.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # data = json.load(open("C:/Users/Steinium/Documents/MS code/pyadv/dictionary/dictionary.json", 'r'))
     # WordList = [data[i]["word"].lower() for i in range(len(data)) if (len(data[i]["word"]) >= 5) and (not any(a in list(data[i]["word"]) for a in string.punctuation))]
     
-    with open("C:/Users/Steinium/Documents/MS code/pyadv/dictionary/dictionary.json", 'r') as fp:
+    with open("dictionary.json", 'r') as fp:
         data = json.load(fp)
         WordList = [data[i]["word"].lower() for i in range(len(data)) if (len(data[i]["word"]) >= 5) and (not any(a in list(data[i]["word"]) for a in string.punctuation))]
 
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     ThatDictionary = {'7': -21, '6': -9, '5': 1, '4': 7, '3': 19, '2': 26, '1': 38, '0': 49}
 
     core = 0
-    for a in range(1, 11):
+    for a in range(1, 2):
         num = random.randint(0,len(WordList))
         word = WordList[num]
         
@@ -40,7 +40,7 @@ if __name__ == '__main__':
         missed = []
         chance = 0
 
-        print(f'********{a}/10********                    Difficulty: I don\'t know :) ')
+        print(f'********{a}/1********                    Difficulty: I don\'t know :) ')
         print(' ',*structure,"\n", ' chances left:', (7-chance),f'\t \t \t \tScore: {core}/1000\n')
 
         while chance <= 7:
@@ -58,7 +58,7 @@ if __name__ == '__main__':
                 chance += 1 
                 varNum = chance if chance <= 7 else 7
 
-            print(f'********{a}/10********                    Difficulty: I don\'t know :) ')
+            print(f'********{a}/1********                    Difficulty: I don\'t know :) ')
             print(' ',*structure,"\n", ' chances left:', (7-varNum),f'\t \t \t \t Score: {core}/1000\n')
             print('Used Letters: ', *missed)
             
